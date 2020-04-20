4f69068ea60c3273cae816eb08521338019f7918
Mike A <jimalik2525@gmail.com>
2020-04-20 11:08:03 -0400

    Updated dev-env and bot can now send messages

--- slack_client.py
+++ slack_client.py
@@ -3,7 +3,24 @@
 A standalone Slack client implementation
 see https://slack.dev/python-slackclient/
 """
-import slack
+
+import os
+from slack.web.client import WebClient
+from slack.errors import SlackApiError
+from dotenv import load_dotenv
+
+load_dotenv()
+
+bot_user_token = os.environ["BOT_USER_TOKEN"]
+client = WebClient(token=bot_user_token)
+# try:
+response = client.chat_postMessage(
+    channel="bot-safehouse",
+    text=":tada: How may I serve you master?"
+)
+# except SlackApiError as e:
+#     # You will get a SlackApiError if "ok" is False
+#     assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
 
 
 # class SlackClient:
